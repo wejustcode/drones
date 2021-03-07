fdcdc506334b384e5561b34c5b902980ce0ce566
Joe Alongi <collectedview@gmail.com>
2021-03-07 13:59:59 -0500

    Drones: Version 2
    Mobile Menu, Back To Top, and Footer update for devices. Meta data updated. JustCode branding added.
    
    Navbar & Mobile:
    Navbar functionality for scrolling. Fixes for adjusting YouTube player on mobile. Nav, Responsive Fallback Headers, Play Button & Page Top.
    
    Isolated Styling:
    Seperate styling for Home and Showcase  pages.
    
    Breakpoints Improved:
    Navigation breakpoints updated. Fallback headers for tablet and mobile. Added play button for scroll down. New page top button. Media Adjust adapted.
    
    Social Share Preview
    
    Video Tooltip & Main Section Background
    
    Trailing Link for GitHub Pages
    
    GitHub Links
    
    Logo Links
    
    Removed Loader
    
    Drones GitHub Links
    
    Update URL

--- js/airborne-media.js
+++ js/airborne-media.js
@@ -1,55 +1,66 @@
 (function($) {
-    "use strict"; // Start of use strict
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
-
-    // Initialize and Configure Scroll Reveal Animation
-    window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal('.sr-contact', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 300);
-
-
-    $(document).ready(function () {
-
-    $(".player").mb_YTPlayer();
-
-    });
-
-})(jQuery); // End of use strict
+	"use strict";
+	// Start of use strict
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: ($($anchor.attr('href')).offset().top - 50)
+		}, 1250, 'easeInOutExpo');
+		event.preventDefault();
+	});
+	// Highlight the top nav as scrolling occurs
+	$('body').scrollspy({
+		target: '.navbar-fixed-top',
+		offset: 51
+	});
+	// Closes the Responsive Menu on Menu Item Click
+	$('.navbar-collapse ul li a').click(function() {
+		$('.navbar-toggle:visible').click();
+	});
+	// Offset for Main Navigation
+	$('#mainNav').affix({
+		offset: {
+			top: 100
+		}
+	})
+	// Initialize and Configure Scroll Reveal Animation
+	window.sr = ScrollReveal();
+	sr.reveal('.sr-icons', {
+		duration: 600,
+		scale: 0.3,
+		distance: '0px'
+	}, 200);
+	sr.reveal('.sr-button', {
+		duration: 1000,
+		delay: 200
+	});
+	sr.reveal('.sr-contact', {
+		duration: 600,
+		scale: 0.3,
+		distance: '0px'
+	}, 300);
+	$(document).ready(function() {
+		$(".player").mb_YTPlayer();
+	});
+	// Back to Top
+	$(document).ready(function() {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function() {
+			$('#back-to-top').tooltip('hide');
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+		$('#back-to-top').tooltip('show');
+	});
+})(jQuery);
+// End of use strict
